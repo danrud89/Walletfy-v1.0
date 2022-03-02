2c019190c4c06e1be45710ee20153b832e4a379a
Daniel <d.rudnik@vp.pl>
2022-03-02 12:00:30 +0100

    add_front_router_class

---
+++ Core/Router.php
@@ -0,0 +1,48 @@
+<?php
+namespace Core;
+
+/**
+ * Router
+ *
+ * PHP version 5.4
+ */
+class Router
+{
+
+    /**
+     * Associative array of routes (the routing table)
+     * @var array
+     */
+    protected $routes = [];
+
+    /**
+     * Parameters from the matched route
+     * @var array
+     */
+    protected $params = [];
+
+    /**
+     * Add a route to the routing table
+     *
+     * @param string $route  The route URL
+     * @param array  $params Parameters (controller, action, etc.)
+     *
+     * @return void
+     */
+    public function add($route, $params = [])
+    {
+        // Convert the route to a regular expression: escape forward slashes
+        $route = preg_replace('/\//', '\\/', $route);
+
+        // Convert variables e.g. {controller}
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+
+        // Convert variables with custom regular expressions e.g. {id:\d+}
+        $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+
+        // Add start and end delimiters, and case insensitive flag
+        $route = '/^' . $route . '$/i';
+
+        $this->routes[$route] = $params;
+    }
+}
