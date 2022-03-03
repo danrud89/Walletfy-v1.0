e4eb202814c5bd617f12c005055fbc8dd27e38ce
Daniel <d.rudnik@vp.pl>
2022-03-03 09:42:06 +0100

    feature/modify_twig_loader

--- Core/View.php
+++ Core/View.php
@@ -45,7 +45,7 @@ class View
 
         if ($twig === null) {
             $loader = new \Twig\Loader\FilesystemLoader(dirname(__DIR__) . '/App/Views');
-            $twig = new \Twig_Environment($loader);
+            $twig = new \Twig\Environment($loader);
         }
 
         echo $twig->render($template, $args);
