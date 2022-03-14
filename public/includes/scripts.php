3fce2798b9a694b3f6568b4867ef659190fd4fa4
Daniel <d.rudnik@vp.pl>
2022-03-14 11:14:33 +0100

    feature/modify_base_view_&_add_mainMenu_view

--- public/includes/scripts.php
+++ public/includes/scripts.php
@@ -55,3 +55,66 @@ unset($_SESSION['serwerStatus']);
 unset($_SESSION['serwerStatusCode']);
 }
 ?>
+
+<script src="script.js"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<script>
+$(document).ready(function() {
+    let wasShown = '<?php echo $_SESSION['logged_in'];?>';
+    if (!wasShown) {
+        var toastMixin = Swal.mixin({
+            toast: true,
+            icon: 'success',
+            title: 'General Title',
+            animation: false,
+            position: 'bottom-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        });
+        toastMixin.fire({
+            title: 'Signed in Successfully'
+        });
+        <?php $_SESSION['logged_in'] = true;?>
+        wasShown = '<?php echo $_SESSION['logged_in'];?>';
+    }
+});
+</script>
+
+<?php 
+if(isset($_SESSION['expenseStatus']) && $_SESSION['expenseStatus'] != '')
+{
+?>
+<script>
+    Swal.fire({
+       title: 'Well done!',
+       text: '<?php echo $_SESSION['expenseStatus']; ?>',
+       icon: '<?php echo $_SESSION['expenseStatusCode']; ?>', 
+     });
+</script>
+<?php
+unset($_SESSION['expenseStatus']);
+unset($_SESSION['expenseStatusCode']);
+}
+?>
+<?php 
+if(isset($_SESSION['incomeStatus']) && $_SESSION['incomeStatus'] != '')
+{
+?>
+<script>
+    Swal.fire({
+       title: 'Well done!',
+       text: '<?php echo $_SESSION['incomeStatus']; ?>',
+       icon: '<?php echo $_SESSION['incomeStatusCode']; ?>', 
+     });
+</script>
+<?php
+unset($_SESSION['incomeStatus']);
+unset($_SESSION['incomeStatusCode']);
+}
+?>
